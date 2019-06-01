d564bf27d4cc322e3a0443e47cc2a705c98899dd
nick200317 <nikpley@mail.ru>
2019-06-01 15:21:49 +0300

    Форма почти подготовлена,написан метод для кнопки "Старт" и метод для генерации изображений.

--- TwentyOne/TwentyOne/Form1.Designer.cs
+++ TwentyOne/TwentyOne/Form1.Designer.cs
@@ -29,13 +29,74 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbl_1 = new System.Windows.Forms.Label();
+            this.lbl_2 = new System.Windows.Forms.Label();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.btn_Give = new System.Windows.Forms.Button();
+            this.btn_End = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lbl_1
+            // 
+            this.lbl_1.AutoSize = true;
+            this.lbl_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbl_1.Location = new System.Drawing.Point(8, 225);
+            this.lbl_1.Name = "lbl_1";
+            this.lbl_1.Size = new System.Drawing.Size(115, 24);
+            this.lbl_1.TabIndex = 0;
+            this.lbl_1.Text = "Ваша рука:";
+            // 
+            // lbl_2
+            // 
+            this.lbl_2.AutoSize = true;
+            this.lbl_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbl_2.Location = new System.Drawing.Point(12, 9);
+            this.lbl_2.Name = "lbl_2";
+            this.lbl_2.Size = new System.Drawing.Size(182, 24);
+            this.lbl_2.TabIndex = 1;
+            this.lbl_2.Text = "Рука противника:";
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_Start.Location = new System.Drawing.Point(12, 170);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(111, 52);
+            this.btn_Start.TabIndex = 2;
+            this.btn_Start.Text = "Старт";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // btn_Give
+            // 
+            this.btn_Give.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_Give.Location = new System.Drawing.Point(129, 170);
+            this.btn_Give.Name = "btn_Give";
+            this.btn_Give.Size = new System.Drawing.Size(121, 51);
+            this.btn_Give.TabIndex = 3;
+            this.btn_Give.Text = "Попросить ещё";
+            this.btn_Give.UseVisualStyleBackColor = true;
+            // 
+            // btn_End
+            // 
+            this.btn_End.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_End.Location = new System.Drawing.Point(256, 170);
+            this.btn_End.Name = "btn_End";
+            this.btn_End.Size = new System.Drawing.Size(113, 50);
+            this.btn_End.TabIndex = 4;
+            this.btn_End.Text = "Закончить";
+            this.btn_End.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(394, 418);
+            this.ClientSize = new System.Drawing.Size(383, 418);
+            this.Controls.Add(this.btn_End);
+            this.Controls.Add(this.btn_Give);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.lbl_2);
+            this.Controls.Add(this.lbl_1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -43,10 +104,17 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Двадцать Одно";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbl_1;
+        private System.Windows.Forms.Label lbl_2;
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.Button btn_Give;
+        private System.Windows.Forms.Button btn_End;
     }
 }
 
--- TwentyOne/TwentyOne/Form1.cs
+++ TwentyOne/TwentyOne/Form1.cs
@@ -12,9 +12,56 @@ namespace TwentyOne
 {
     public partial class Form1 : Form
     {
+        Random rnd = new Random();
+        List<string> list = new List<string>(); //Списко для изображения карт
+        
         public Form1()
         {
             InitializeComponent();
+            GenerateImage();
+        }
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            Start();
+        }
+
+        //Заливаем в наш список пути с изображениями карт
+        private void GenerateImage()
+        {
+            list.Add("C:\\Users\\nikpl\\Desktop\\для игры\\двадцать одно\\6vini.jpg");
+            list.Add("C:\\Users\\nikpl\\Desktop\\для игры\\двадцать одно\\6tref.PNG");
+            list.Add("C:\\Users\\nikpl\\Desktop\\для игры\\двадцать одно\\6bubi.gif");
+        }
+
+        //Метод для добавлении первых двух случайных карт
+        private void Start()
+        {
+            int res;
+            int x = 25;
+            int y = 50;
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    res = rnd.Next(3);
+                    PictureBox pic2 = new PictureBox();
+                    pic2.Location = new Point(x, y);
+                    pic2.BorderStyle = BorderStyle.FixedSingle;
+                    pic2.Size = new Size(50, 100);
+                    pic2.BackColor = SystemColors.Control;
+                    pic2.SizeMode = PictureBoxSizeMode.StretchImage;
+                    pic2.TabStop = false;
+                    pic2.Load(list[res]);
+                    x += 50;
+                    Controls.Add(pic2);
+
+                }
+
+                y = 275;
+                x = 25;
+            }
         }
     }
 }
